2fdbf46e002197f8768a6a6bdac3d019a316625a
James Jordan <30762515+abcjamesjordan@users.noreply.github.com>
2021-03-14 18:21:59 -0600

    Major update day 2
    Most of data organization is complete. Working towards plotting the data and compiling a PDF document.

--- get_data.py
+++ get_data.py
@@ -1,24 +1,73 @@
 import twint
 import pandas as pd
 
-# Configure the twint scrapper
-c = twint.Config()
-c.Username = 'SPGlobalPlatts'
-c.Limit = 2000
-c.Pandas = True
-c.Hide_output = True
-c.Retweets = True
-c.Filter_retweets = False
+from sklearn.preprocessing import MultiLabelBinarizer
 
-# Run the twint twitter scraper function
-twint.run.Search(c)
+def tweets_scrape(username, pandas_bool=True, hide_output_bool=True, retweets_bool=True, filter_retweets_bool=False):
+    c = twint.Config()
+    c.Username = username
+    c.Limit = 2000
+    c.Pandas = pandas_bool
+    c.Hide_output = hide_output_bool
+    c.Retweets = retweets_bool
+    c.Filter_retweets = filter_retweets_bool
+    
+    # Run the twint twitter scraper function
+    twint.run.Search(c)
 
-# Store the results into a pandas dataframe
-tweets_df = twint.storage.panda.Tweets_df
+    # Store the results into a pandas dataframe
+    tweets_df = twint.storage.panda.Tweets_df
 
-# Remove unncessary data from the dataframe
-tweets_df.drop(columns=['id', 'conversation_id', 'created_at', 'timezone', 'place', 'language', 'cashtags', 'user_id', 'user_id_str', 'username', 'name', 'link', 'thumbnail', 'retweet', 'quote_url', 'search', 'near',
-                        'geo', 'source', 'user_rt_id', 'user_rt', 'retweet_id', 'retweet_date', 'translate', 'trans_src', 'trans_dest'], inplace=True)
+    # Remove unncessary data from the dataframe
+    tweets_df.drop(columns=['id', 'conversation_id', 'created_at', 'timezone', 'place', 'language', 'cashtags', 'user_id', 'user_id_str', 'username', 'name', 'link', 'thumbnail', 'retweet', 'quote_url', 'search', 'near',
+                            'geo', 'source', 'user_rt_id', 'user_rt', 'retweet_id', 'retweet_date', 'translate', 'trans_src', 'trans_dest'], inplace=True)
+    
+    return tweets_df
 
-# Export the pandas dataframe to a pickle file
-tweets_df.to_pickle('./tweets_df.pkl')
+def profile_lookup(username, pandas_bool=True, hide_output_bool=True):
+    c = twint.Config()
+    c.Username = username
+    c.Pandas = pandas_bool
+    c.Hide_output = hide_output_bool
+
+    twint.run.Lookup(c)
+    
+    user_df = twint.storage.panda.User_df
+    user_df.to_pickle('./user_df.pkl')
+    
+    return user_df
+
+# Get the original company twitter data
+df_tweets = tweets_scrape('SPGlobalPlatts')
+df_tweets.to_pickle('./tweets_df.pkl')
+df_user = profile_lookup('SPGlobalPlatts')
+
+
+# 1. Find top 3 mentions within those tweets
+twitter_handle_regex = r'@([a-zA-Z0-9_]+)'
+df_tweets['mentions'] = df_tweets['tweet'].str.findall(twitter_handle_regex).apply(','.join)
+df_tweets['mentions'] = df_tweets['mentions'].str.split(',')
+
+s = df_tweets['mentions']
+mlb = MultiLabelBinarizer()
+df_mentions = pd.DataFrame(mlb.fit_transform(s), columns=mlb.classes_)
+sum_mentions = df_mentions.sum(axis=0, skipna=True).sort_values(ascending=False)
+sum_mentions.pop(str(df_user['username'].iloc[0]))
+sum_mentions.pop('')
+
+# 1a. Export the top mentions
+top_3_mentions = sum_mentions[0:3].index.tolist()
+print(top_3_mentions)
+
+# 2. Scrape same data from those three sources
+tweets_mention_1 = tweets_scrape(top_3_mentions[0])
+tweets_mention_2 = tweets_scrape(top_3_mentions[1])
+tweets_mention_3 = tweets_scrape(top_3_mentions[2])
+
+# 3. Return that data into new variables
+tweets_mention_1.to_pickle('./tweets_mention1_df.pkl')
+tweets_mention_2.to_pickle('./tweets_mention2_df.pkl')
+tweets_mention_3.to_pickle('./tweets_mention3_df.pkl')
+with open('top_3_mentions.txt', 'w') as f:
+    for item in top_3_mentions:
+        f.write("%s\n" % item)
--- organize_data.py
+++ organize_data.py
@@ -1,8 +1,19 @@
 import pandas as pd
 import numpy as numpy
 import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+from wordcloud import WordCloud
 
 from sklearn.preprocessing import MultiLabelBinarizer
+
+# Constants
+DAYS_OF_WEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+MONTHS_OF_YEAR = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+# Regex variables
+twitter_handle_regex = r'@([a-zA-Z0-9_]+)'
 
 def create_sums_variable(df_sums, mentions=False, hashtags=False):
     '''
@@ -80,65 +91,163 @@ def average_tweets_by(df, unit_of_time):
     else:
         print('please use either day, hour, week, month, or year')
     
+def organize_df(df):
+    ''' 
+    Organize each dataframe. Returns dataframe, mentions sum df, and hashtag sum df.
+    '''
+    # Fix the dates
+    df['date'] = pd.to_datetime(df['date'], utc=True)
+    df['week'] = df['date'].dt.isocalendar().week
+    df['month'] = df['date'].dt.month
+    df['year'] = df['date'].dt.year
+    
+    # Add mentions column similar to hashtags column
+    twitter_handle_regex = r'@([a-zA-Z0-9_]+)'
+    df['mentions'] = df['tweet'].str.findall(twitter_handle_regex).apply(','.join)
+    df['mentions'] = df['mentions'].str.split(',')
+    
+    # Create the summing variables for mentions and hashtags
+    sum_mentions = create_sums_variable(df, mentions=True)
+    sum_hashtags = create_sums_variable(df, hashtags=True)
+    
+    # Reorder dataframe
+    df = df.sort_values(by='date').reset_index(drop=True)
+    
+    # Add counter column
+    df['count'] = df.index + 1
+    
+    return df, sum_mentions, sum_hashtags
 
-# Import that dataframe from the get_day.py output
-path = os.getcwd()
-file_path = os.path.join(path, 'tweets_df.pkl')
-df = pd.read_pickle(file_path)
+def import_dataframes():
+    ''' 
+    Imports all dataframes from pickle files
+    '''
+    path = os.getcwd()
+    file_path_tweets = os.path.join(path, 'tweets_df.pkl')
+    file_path_user = os.path.join(path, 'user_df.pkl')
+    file_path_mention1 = os.path.join(path, 'tweets_mention1_df.pkl')
+    file_path_mention2 = os.path.join(path, 'tweets_mention2_df.pkl')
+    file_path_mention3 = os.path.join(path, 'tweets_mention3_df.pkl')
+    df = pd.read_pickle(file_path_tweets)
+    df_user = pd.read_pickle(file_path_user)
+    df_mention1 = pd.read_pickle(file_path_mention1)
+    df_mention2 = pd.read_pickle(file_path_mention2)
+    df_mention3 = pd.read_pickle(file_path_mention3)
+    
+    return df, df_user, df_mention1, df_mention2, df_mention3
 
-# Constants
-DAYS_OF_WEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-MONTHS_OF_YEAR = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+def create_per_df(df):
+    '''
+    Returns all dataframes of per_blank statistics (day, hour, dayofweek, week, month, year)
+    '''
+    per_day = get_tweets_by(df, 'day')
+    per_hour = get_tweets_by(df, 'hour')
+    per_dayofweek = get_tweets_by(df, 'dayofweek')
+    per_week = get_tweets_by(df, 'week')
+    per_month = get_tweets_by(df, 'month')
+    per_year = get_tweets_by(df, 'year')
+    
+    return per_day, per_hour, per_dayofweek, per_week, per_month, per_year
 
-NUM_OF_TWEETS = len(df)
-TOTAL_LIKES = df['nlikes'].sum()
-TOTAL_REPLIES = df['nreplies'].sum()
-TOTAL_RETWEETS = df['nretweets'].sum()
+# Import dataframes
+df, df_user, df_mention1, df_mention2, df_mention3 = import_dataframes()
 
-# Regex variables
-twitter_handle_regex = r'@([a-zA-Z0-9_]+)'
+# Import top 3 mentions
+top_3_mentions = []
+with open('top_3_mentions.txt', 'r') as f:
+    for line in f:
+        top_3_mentions.append(str(line).replace('\n', ''))
 
-# Fix the date column to US/Eastern Time Zone and add date variables
-df['date'] = pd.to_datetime(df['date'], utc=True)
-df['date'] = pd.to_datetime(df['date']).dt.tz_convert('US/Eastern')
-df['week'] = df['date'].dt.isocalendar().week
-df['month'] = df['date'].dt.month
-df['year'] = df['date'].dt.year
+# Organize all datasets
+df, sum_mentions, sum_hashtags = organize_df(df)
+df['type'] = str('@'+df_user['name'].iloc[0])
+df_mention1, sum_mentions1, sum_hashtags1 = organize_df(df_mention1)
+df_mention1['type'] = '@'+top_3_mentions[0]
+df_mention2, sum_mentions2, sum_hashtags2 = organize_df(df_mention2)
+df_mention2['type'] = '@'+top_3_mentions[1]
+df_mention3, sum_mentions3, sum_hashtags2 = organize_df(df_mention3)
+df_mention3['type'] = '@'+top_3_mentions[2]
 
-# Create a mentions column similar to the hashtags column
-df['mentions'] = df['tweet'].str.findall(twitter_handle_regex).apply(','.join)
-df['mentions'] = df['mentions'].str.split(',')
 
-# Create the summing variables for mentions and hashtags
-sum_mentions = create_sums_variable(df, mentions=True)
-sum_hashtags = create_sums_variable(df, hashtags=True)
+# # Create various dataframes for plotting analysis
+tweets_per_day, tweets_per_hour, tweets_per_dayofweek, tweets_per_week, tweets_per_month, tweets_per_year = create_per_df(df)
+tweets_per_day1, tweets_per_hour1, tweets_per_dayofweek1, tweets_per_week1, tweets_per_month1, tweets_per_year1 = create_per_df(df_mention1)
+tweets_per_day2, tweets_per_hour2, tweets_per_dayofweek2, tweets_per_week2, tweets_per_month2, tweets_per_year2 = create_per_df(df_mention2)
+tweets_per_day3, tweets_per_hour3, tweets_per_dayofweek3, tweets_per_week3, tweets_per_month3, tweets_per_year3 = create_per_df(df_mention3)
 
-# Create various dataframes for plotting analysis
-tweets_per_day = get_tweets_by(df, 'day')
-tweets_per_hour = get_tweets_by(df, 'hour')
-tweets_per_week_day = get_tweets_by(df, 'dayofweek')
-tweets_per_week = get_tweets_by(df, 'week')
-tweets_per_month = get_tweets_by(df, 'month')
-tweets_per_year = get_tweets_by(df, 'year')
+# Master dataframes for plotting
+df_master = pd.concat([df, df_mention1, df_mention2, df_mention3]).reset_index(drop=True)
 
-# //FIXME fix the data statstics
-# Database statistics
+### Plotting section
+# Tweets over time
+# sns.set_context('poster')
+# sns.relplot(data=df_master, x='date', y='count', kind='line', hue='type', height=10, aspect=1.5, legend=True)
+# plt.title('Total Tweets vs Top 3 Mentions')
+# plt.xlabel('Time')
+# plt.ylabel('# of Tweets')
+# plt.show()
+
+# Wordclouds
+# Mentions
+mentions_dict = sum_mentions.to_dict()
+wordcloud = WordCloud(width=1600, height=800).generate_from_frequencies(mentions_dict)
+
+plt.figure(figsize = (20,10), dpi=600, facecolor='k')
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.tight_layout(pad=0)
+plt.show()
+
+# Hashtags
+
+
+
+
+
+
+
+
+
+# # Database Stats
+# NUM_OF_TWEETS = len(df)
+# TOTAL_LIKES = df['nlikes'].sum()
+# TOTAL_REPLIES = df['nreplies'].sum()
+# TOTAL_RETWEETS = df['nretweets'].sum()
+
+
+
+
+
+
+
+# # test_df = df.copy()
+# # test_df['count'] = 1
+# # print(test_df.groupby(['month']).mean())
+# # print(test_df.groupby(['month']).sum().iloc[:, -1].mean())
+# # print(test_df.columns)
+# # print(sum_mentions)
+
+# # //FIXME fix the data statstics
+# # Database statistics
 # AVG_TWEETS_PER_DAY = tweets_per_day['count'].mean()
-# AVG_TWEETS_PER_HOUR = tweets_per_hour['count'].mean()
-# AVG_TWEETS_PER_WEEK = tweets_per_week['count'].mean()
-# AVG_TWEETS_PER_MONTH = tweets_per_month['count'].mean()
-# AVG_TWEETS_PER_YEAR = tweets_per_year['count'].mean()
-# print('Number of tweets', NUM_OF_TWEETS)
-# print(AVG_TWEETS_PER_DAY, AVG_TWEETS_PER_HOUR, AVG_TWEETS_PER_WEEK, AVG_TWEETS_PER_MONTH, AVG_TWEETS_PER_YEAR)
+# # AVG_TWEETS_PER_HOUR = tweets_per_hour['count'].mean()
+# # AVG_TWEETS_PER_WEEK = tweets_per_week['count'].mean()
+# # AVG_TWEETS_PER_MONTH = tweets_per_month['count'].mean()
+# # AVG_TWEETS_PER_YEAR = tweets_per_year['count'].mean()
+# # print('Number of tweets', NUM_OF_TWEETS)
+# # print(AVG_TWEETS_PER_DAY, AVG_TWEETS_PER_HOUR, AVG_TWEETS_PER_WEEK, AVG_TWEETS_PER_MONTH, AVG_TWEETS_PER_YEAR)
+
+# # print('Tweets per day', AVG_TWEETS_PER_DAY)
 
 
+# # User statistics:
+# print(df_user.columns)
+# NUM_OF_FOLLOWERS = df_user['followers'].iloc[0]
+# USER_NAME = df_user['name'].iloc[0]
+# JOIN_DATE = df_user['join_date'].iloc[0]
+# NUM_OF_TWEETS_ACTUAL = df_user['tweets'].iloc[0]
 
-
-
-# test_df = df.copy()
-# test_df['count'] = 1
-# test_df['month'] = df['date'].dt.month
-# print(test_df.groupby(['month']).mean())
-# print(test_df.groupby(['month']).sum().iloc[:, 5])
-
-
+# print(USER_NAME)
+# print(f'Number of Followers: {NUM_OF_FOLLOWERS}')
+# print(f'Join Date: {JOIN_DATE}')
+# print(f'Number of Tweets: {NUM_OF_TWEETS_ACTUAL}')
